c9fa389a667ebf2c06e3a502db8e961a01151a01
jason-zl190 <i.liangzisheng@gmail.com>
2020-11-06 00:34:46 +0000

    first commit

--- tensorflow_datasets/image_classification/gradient_xray_tuberculosis_frontal.py
+++ tensorflow_datasets/image_classification/gradient_xray_tuberculosis_frontal.py
@@ -11,9 +11,7 @@ import io
 import os
 import pandas as pd
 import numpy as np
-import pydicom
-from PIL import Image
-# import gh_crypt
+
 
 # TODO(gradient_xray_tuberculosis_frontal): BibTeX citation
 _CITATION = """
@@ -23,7 +21,9 @@ _CITATION = """
 _DESCRIPTION = """
 """
 
-csv_path = '../tb_frontal.csv'
+train_csv_path = '../train.csv'
+val_csv_path = '../val.csv'
+test_csv_path = '../test.csv'
 
 class GradientXrayTuberculosisFrontal(tfds.core.GeneratorBasedBuilder):
   """TODO(gradient_xray_tuberculosis_frontal): Short description of my dataset."""
@@ -43,6 +43,8 @@ class GradientXrayTuberculosisFrontal(tfds.core.GeneratorBasedBuilder):
           tfds.features.Image(shape=(None, None, 1),
                               dtype=tf.uint16,
                               encoding_format='png'),
+          "label":
+          tfds.features.ClassLabel(names=["negtive", "positive"]),
         }),
         # If there's a common (input, target) tuple from the features,
         # specify them here. They'll be used if as_supervised=True in
@@ -57,19 +59,28 @@ class GradientXrayTuberculosisFrontal(tfds.core.GeneratorBasedBuilder):
     """Returns SplitGenerators."""
 
 
-    df = pd.read_csv(csv_path, encoding='utf-8', lineterminator='\n')
+    train_df = pd.read_csv(train_csv_path, lineterminator='\n')
+    val_df = pd.read_csv(val_csv_path, lineterminator='\n')
+    test_df = pd.read_csv(test_csv_path, lineterminator='\n')
+
     
     return [
       tfds.core.SplitGenerator(
           name=tfds.Split.TRAIN,
           gen_kwargs={
-            'df':df[df['split']=='train'],
+            'df':train_df,
           },
       ),
       tfds.core.SplitGenerator(
           name=tfds.Split.VALIDATION,
           gen_kwargs={
-            'df':df[df['split']=='validation'],
+            'df':val_df,
+          },
+      ),
+      tfds.core.SplitGenerator(
+          name=tfds.Split.TEST,
+          gen_kwargs={
+            'df':test_df,
           },
       ),
     ]
@@ -77,26 +88,25 @@ class GradientXrayTuberculosisFrontal(tfds.core.GeneratorBasedBuilder):
   def _generate_examples(self, df):
     """Yields examples."""
 
-    raise AssertionError('To use this dataset, a valid `data_dir` should be provided')
+    for idx, row in df.iterrows():    
+#       path =tf.constant('oss://ai-dicom\x01id={access_key}\x02key={secret_key}\x02host=oss-cn-hangzhou-internal.aliyuncs.com/{path}'.format(
+#                   bucket='ai-dicom',
+#                   path='data/encrypted_dicoms/' + row.path,
+#                   access_key=os.environ.get("OSS_ACCESS_KEY"),
+#                   secret_key=os.environ.get("OSS_SECRET_KEY")
+#               ))
 
-    # for idx, row in df.iterrows():    
-    #   path =tf.constant('oss://ai-dicom\x01id={access_key}\x02key={secret_key}\x02host=oss-cn-hangzhou-internal.aliyuncs.com/{path}'.format(
-    #               bucket='ai-dicom',
-    #               path='data/encrypted_dicoms/' + row.path,
-    #               access_key=os.environ.get("OSS_ACCESS_KEY"),
-    #               secret_key=os.environ.get("OSS_SECRET_KEY")
-    #           ))
-
-      # image_bytes = gh_crypt.decrypt_bytes(
-      #   contents=tf.io.read_file(path),
-      #   chunk_size=64*1024, 
-      #   key_hex=os.environ['DECRYPT_KEY'])
+#       image_bytes = gh_crypt.decrypt_bytes(
+#         contents=tf.io.read_file(path),
+#         chunk_size=64*1024, 
+#         key_hex=os.environ['DECRYPT_KEY'])
       
-      # image_tensor = tfio.image.decode_dicom_image(image_bytes)
-      # image_tensor = tf.cast(tf.round(tf.image.resize_with_pad(image_tensor[0], 512, 512)), tf.uint16)
+#       image_tensor = tfio.image.decode_dicom_image(image_bytes)
+#       image_tensor = tf.cast(tf.round(tf.image.resize_with_pad(image_tensor[0], 512, 512)), tf.uint16)
 
-      # record = {
-      #   "image/name": row.path,
-      #   "image": image_tensor.numpy(),
-      # }
-      # yield idx, record
+      record = {
+        "image/name": row.path,
+        "image": image_tensor.numpy(),
+        "label": 'negtive' if row.label == 0 else 'positive'
+      }
+      yield idx, record
